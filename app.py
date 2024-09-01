8841a1c1c7db340e31a7657f366632dc790d70b9
zubair819 <164842642+zubair819@users.noreply.github.com>
2024-09-01 21:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -263,5 +263,4 @@ def view_financial_data_tables():
 
     return render_template('view_financial_data_tables.html', financial_data=user_data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':app.run(debug=True)
